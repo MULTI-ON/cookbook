046d4b7aaffb630f327947470374f3bd5b9cb410
Karthik Raja <karthikraja2k1@gmail.com>
2023-07-11 09:01:41 +0530

    test file to test multion-js

--- multion-js/test.js
+++ multion-js/test.js
@@ -1,7 +1,10 @@
 import Multion from "./main.js";
 
-
-const multion = new Multion();
-// multion.login();
-// console.log(multion.token,"jo");
-multion.newSession("show sun pictures");
+async function run() {
+    const multion = new Multion();
+    const h = await multion.login(); // Wait for login to complete
+    console.log("j",h,"j");
+    await multion.newSession({ input: "show sun pictures" });
+  }
+  
+  run()
