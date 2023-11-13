c6285e45476c7af9a212f7de023869dd3fd20b56
Div Garg <divyansh199@gmail.com>
2023-11-13 01:31:36 -0800

    fix js sdk

---
+++ sdks/multion-js/dist/index.js
@@ -0,0 +1,12 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "Multion", {
+  enumerable: true,
+  get: function () {
+    return _multion.Multion;
+  }
+});
+var _multion = require("./multion.js");
---
+++ sdks/multion-js/src/index.js
@@ -0,0 +1,7 @@
+import { Multion } from "./multion.js";
+// import { MultionToolSpec } from "./browse.js";
+
+export {
+  Multion,
+  //   MultionToolSpec,
+};
