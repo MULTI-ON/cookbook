060d920c59e01e69ae033ac78f8c8a0fff3370f9
Howard Gil <howardbgil@gmail.com>
2024-03-05 17:57:11 -0800

    Fixed bug where event duration was 0

--- sdks/multion-py/multion/agentops_client.py
+++ sdks/multion-py/multion/agentops_client.py
@@ -1,6 +1,7 @@
 from agentops import Client, Event
 from typing import List
 
+
 class AgentOpsClient:
     def __init__(self, api_key: str = None, org_key: str = None, tags: List[str] = ["prod"]):
         self.current_event = Event(event_type="api")
@@ -15,8 +16,8 @@ class AgentOpsClient:
 
     def record(self, **kwargs):
         if self.client:
-            self.current_event = Event(
-                event_type=self.current_event.event_type, action_type=self.current_event.action_type, **kwargs)
+            self.current_event = Event(event_type=self.current_event.event_type, action_type=self.current_event.action_type,
+                                       init_timestamp=self.current_event.init_timestamp, ** kwargs)
             self.client.record(self.current_event)
 
     def end_session(self, video_url: None, end_state="Success"):
